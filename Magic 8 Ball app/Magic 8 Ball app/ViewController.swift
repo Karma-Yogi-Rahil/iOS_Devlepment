deaa086cfce69c93e8932d32b7bc45e343e7ff87
Rahil <rahilmehta18@gmail.com>
2021-07-27 09:59:21 +0530

    added magic ball app

---
+++ Magic 8 Ball app/Magic 8 Ball app/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball app
+//
+//  Created by Rahil Mehta on 27/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var ballImage: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    @IBAction func ask(_ sender: Any) {
+        
+        let ballImage_array = [ #imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball4")]
+        
+        ballImage.image = ballImage_array.randomElement()
+        
+    }
+    
+    
+    
+
+}
+
